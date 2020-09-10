9129f49977e9bce6b162aed78e1a9967675f4fc9
Zhenya <vasvasjane@gmail.com>
2020-09-10 17:48:33 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.use(express_1.default.static('./dist'));
+const port = 3000;
+app.listen(port, function () {
+    return console.log(`server is listening on ${port}`);
+});
+//# sourceMappingURL=app.js.map
